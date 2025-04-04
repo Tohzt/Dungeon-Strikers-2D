c57c527e7239abbf399255360f79a438d726a43d
tohzt <steven.g.french@live.com>
2025-04-04 00:45:31 -0600

    replaced TextEdit with LineEdit for center alignment

--- Main Menu/main_menu.gd
+++ Main Menu/main_menu.gd
@@ -1,5 +1,4 @@
 extends Node
-@onready var display_name = $CanvasLayer/Multiplayer/VBoxContainer/DisplayName
 
 func _on_host_pressed() -> void: Server.Create()
 func _on_join_pressed() -> void: Server.Join()
@@ -8,7 +7,5 @@ func _on_quit_pressed() -> void: get_tree().quit()
 func _on_quick_play_pressed() -> void:
 	get_tree().change_scene_to_file(Global.GAME)
 
-
-func _on_display_name_text_changed():
-	print("setting diaply: ", display_name.text)
-	Global.player_diplay = display_name.text
+func _on_display_name_text_changed(new_text: String) -> void:
+	Global.player_diplay = new_text
